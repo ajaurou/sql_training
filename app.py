77f2cde56644c79d3c81bed92c7431c9238a9aa3
anthony.jaurou <anthony.jaurou@consulting-for.accor.com>
2026-02-09 11:47:34 +0100

    add database connection

--- app.py
+++ app.py
@@ -1,25 +1,7 @@
-import io
-
 import duckdb
-import pandas as pd
 import streamlit as st
 
-CSV = """
-beverage,price
-orange juice,2.5
-Expresso,2
-Tea,3
-"""
-beverages = pd.read_csv(io.StringIO(CSV))
-
-
-CSV2 = """
-food_item,food_price
-cookie juice,2.5
-chocolatine,2
-muffin,3
-"""
-food_items = pd.read_csv(io.StringIO(CSV2))
+conn = duckdb.connect("data/exercices_sql_tables.duckdb", read_only=False)
 
 
 st.write("""
@@ -28,46 +10,55 @@ Spaced Repetition SQL practice""")
 
 
 with st.sidebar:
-    option = st.selectbox(
+    theme = st.selectbox(
         "What would you like to review?",
-        ("Joins", "Aggregation", "Windows function"),
+        ("cross_joins", "Joins", "Aggregation", "Windows function"),
         placeholder="Select a theme",
     )
 
+    exercices = conn.execute(f"select * from memory_state where theme = '{theme}'").df()
+    st.write(exercices)
 
-SOLUTION_STR = """
-    select * from beverages
-    cross join food_items"""
+# SOLUTION_STR = """
+#     select * from beverages
+#     cross join food_items"""
 
-solution_df = duckdb.query(SOLUTION_STR).df()
+# solution_df = duckdb.query(SOLUTION_STR).df()
 
 st.header("Enter your code:")
 query = st.text_area(label="Your SQL code here", key="user_input")
+
 if query:
-    result = duckdb.query(query).df()
+    result = conn.execute(query).df()
     st.dataframe(result)
 
-    if len(result.columns) != len(solution_df.columns):
-        st.write("Your code does not have the right number of columns")
-    else:
-        st.write("Your code has the right number of columns")
+#     if len(result.columns) != len(solution_df.columns):
+#         st.write("Your code does not have the right number of columns")
+#     else:
+#         st.write("Your code has the right number of columns")
 
-    try:
-        result = result[solution_df.columns]
-        st.dataframe(result.compare(solution_df))
-    except KeyError as e:
-        print(f"{e} - Columns naming is incorrect")
+#     try:
+#         result = result[solution_df.columns]
+#         st.dataframe(result.compare(solution_df))
+#     except KeyError as e:
+#         print(f"{e} - Columns naming is incorrect")
 
 
 tab2, tab3 = st.tabs(["Tables", "Solutions"])
 
 with tab2:
-    st.write("Table: beverages")
-    st.dataframe(beverages)
-    st.write("Table: food_items")
-    st.dataframe(food_items)
-    st.write("Expected:")
-    st.dataframe(solution_df)
+    st.write(exercices.loc[0, "tables"])
+    exercices_tables = exercices.loc[0, "tables"]
+    for table in exercices_tables:
+        st.write(f"Table: {table}")
+        st.dataframe(conn.execute(f"select * from {table}").df())
 
-with tab3:
-    st.write(f"Answer: {SOLUTION_STR}")
+#     st.write("Table: beverages")
+#     st.dataframe(beverages)
+#     st.write("Table: food_items")
+#     st.dataframe(food_items)
+#     st.write("Expected:")
+#     st.dataframe(solution_df)
+
+# with tab3:
+#     st.write(f"Answer: {SOLUTION_STR}")
